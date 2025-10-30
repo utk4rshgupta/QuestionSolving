c41c057c2e9e3ef9d67cd1ee17d133b23627348d
utk4rshgupta <utkarshgupta2803@gmail.com>
2025-10-30 21:50:10 +0530

    Solutions

--- src/secondLargset.java
+++ src/secondLargset.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class secondLargset {
+    static void main(String[] args) {
+        int arr[] = new int[]{20, 34, 40, 22, 34, 76, 90};
+        int first = arr[0], second = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (first < arr[i]) {
+                second = first;
+                first = arr[i];
+            }
+        }
+        System.out.println(second);
+    }
 }
--- src/uniqueChar.java
+++ src/uniqueChar.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class uniqueChar {
+    static void main(String[] args) {
+
+    }
 }
